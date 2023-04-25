0ccaa7454c7d104bd0812f4f2d88e955281fd939
daduong <doananhduong09.10@gmail.com>
2023-04-25 07:29:28 +0700

    init commit

---
+++ Lab_3_carmpg.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 21:53:34 2015
+
+@author: nymph
+"""
+
+
+#################################### Read the data ############################
+import pandas as pd
+from pandas import DataFrame, Series
+import numpy as np
+import matplotlib.pyplot as plt
+
+''' read_csv()
+The read_csv() function in pandas package parse an csv data as a DataFrame data structure. What's the endpoint of the data?
+The data structure is able to deal with complex table data whose attributes are of all data types. 
+Row names, column names in the dataframe can be used to index data.
+'''
+
+data = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data-original", delim_whitespace = True, \
+ header=None, names = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model', 'origin', 'car_name'])
+
+data['mpg']
+data.mpg
+data.iloc[0,:]
+
+print(data.shape)
+
+################################## Enter your code below ######################
+
+# 1. How many cars and how many attributes are in the data set. 
+num_cars, num_attributes = data.shape
+print("Number of cars in the dataset:", num_cars)
+print("Number of attributes in the dataset:", num_attributes)
+
+""" 2. How many distinct car companies are represented in the data set? What is the name of the car
+with the best MPG? What car company produced the most 8-cylinder cars? What are the names
+of 3-cylinder cars? """
+
+# How many distinct car companies are represented in the data set?
+car_companies = data["car_name"].apply(lambda x: x.split()[0])
+num_car_companies = len(car_companies.unique())
+print("Number of distinct car companies in the dataset:", num_car_companies)
+
+# What is the name of the car with the best MPG?
+idx = data["mpg"].idxmax()
+car_name = data.loc[idx, "car_name"]
+print("The car with the best MPG is:", car_name)
+
+# What car company produced the most 8-cylinder cars?
+eight_cyl_cars = data[data["cylinders"] == 8]
+car_companies = eight_cyl_cars["car_name"].apply(lambda x: x.split()[0])
+most_common_company = car_companies.value_counts().idxmax()
+print("The car company that produced the most 8-cylinder cars is:", most_common_company)
+
+# What are the names of 3-cylinder cars?
+three_cyl_cars = data[data["cylinders"] == 3]
+car_names = three_cyl_cars["car_name"].tolist()
+print("The names of the 3-cylinder cars are:")
+for name in car_names:
+    print(name)
+
+# 3 What is the range, mean, and standard deviation of each attribute? Pay attention to potential missing values.
+
+stats = data.describe()
+
+print("Range, mean, and standard deviation of each attribute:")
+for col in stats.columns:
+    attr_range = stats.loc["max", col] - stats.loc["min", col]
+    
+    print(col)
+    print("Range:", attr_range)
+    print("Mean:", stats.loc["mean", col])
+    print("Standard deviation:", stats.loc["std", col])
+    print()
+
+""" 4. Plot histograms for each attribute. Pay attention to the appropriate choice of number of bins.
+Write 2-3 sentences summarizing some interesting aspects of the data by looking at the histograms. """
+
+data.hist(bins=20, figsize=(10,10))
+
+plt.suptitle("Histograms of Auto-MPG Dataset Attributes", fontsize=16)
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+
+plt.show()
+
+# 5
+# 6
+# 7
+plt.figure()
+plt.scatter(data.horsepower, data.mpg)
+plt.xlabel('Horse Power')
+plt.ylabel('Mile per gallon')
+
+plt.figure()
+plt.scatter(data.horsepower, data.acceleration)
+plt.xlabel('Horse Power')
+plt.ylabel('Acceleration')
+
+plt.figure()
+plt.scatter(data.horsepower, data.displacement)
+plt.xlabel('Horse Power')
+plt.ylabel('Displacement')
+
+plt.show()
+# 8
+# 9
+
+# main fuction
---
+++ Lab_3_electricpower.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 02:33:19 2015
+
+@author: nymph
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import numpy as np
+
+
+############################## Your code for loading and preprocess the data ##
+# read data
+df = pd.read_csv('household_power_consumption.txt', delimiter=';',\
+                parse_dates=['Date'], date_parser=(lambda x : pd.to_datetime(x, format='%d/%m/%Y')))
+
+# filtering the date we will process
+df = df.loc[(df.Date == '2007-02-01') | (df.Date == '2007-02-02')]
+
+# preprocess data
+df['datetime'] = pd.to_datetime(df.Date.astype('string') + ' ' + df.Time.astype('string'), format='%Y-%m-%d %H:%M:%S')
+df = df.iloc[:, 2:]
+df[df.columns[:-1]] = df[df.columns[:-1]].astype('float')
+
+############################ Complete the following 4 functions ###############
+def plot1():
+    pass
+
+def plot2():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(df.datetime, df.Global_active_power, c='black')
+    plt.yticks(list(range(0, 7, 2)))
+    plt.ylabel('Global Active Power (kilowatts)')
+    ax.xaxis.set_major_formatter(date_fmt)
+    ax.xaxis.set_major_locator(day_locator)
+    plt.savefig('plot2.png')
+
+def plot3():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(df.datetime, df.Sub_metering_1, c='black', label='Sub_metering_1')
+    plt.plot(df.datetime, df.Sub_metering_2, c='red', label='Sub_metering_2')
+    plt.plot(df.datetime, df.Sub_metering_3, c='blue', label='Sub_metering_3')
+    plt.yticks(list(range(0, 31, 10)))
+    plt.ylabel('Energy sub metering')
+    ax.xaxis.set_major_formatter(date_fmt)
+    ax.xaxis.set_major_locator(day_locator)
+    plt.legend()
+
+    plt.savefig('plot3.png')
+
+def plot4():
+    fig, axes = plt.subplots(2, 2, figsize=(12, 12))
+
+    def set_xticks(ax):
+        ax.xaxis.set_major_formatter(date_fmt)
+        ax.xaxis.set_major_locator(day_locator)
+
+    set_xticks_vectorizer = np.vectorize(set_xticks)
+    set_xticks_vectorizer(axes)
+
+    axes[0, 0].plot(df.datetime, df.Global_active_power, c='black')
+    axes[0, 0].set_yticks(list(range(0, 7, 2)))
+    axes[0, 0].set_ylabel('Global Active Power')
+
+    axes[0, 1].plot(df.datetime, df.Voltage, c='black')
+    axes[0, 1].set_yticks(list(range(234, 247, 4)))
+    axes[0, 1].xaxis.set_major_formatter(date_fmt)
+    axes[0, 1].xaxis.set_major_locator(day_locator)
+    axes[0, 1].set_ylabel('Voltage')
+    axes[0, 1].set_xlabel('datetime')
+
+    axes[1, 0].plot(df.datetime, df.Sub_metering_1, c='black', label='Sub_metering_1')
+    axes[1, 0].plot(df.datetime, df.Sub_metering_2, c='red', label='Sub_metering_2')
+    axes[1, 0].plot(df.datetime, df.Sub_metering_3, c='blue', label='Sub_metering_3')
+    axes[1, 0].set_yticks(list(range(0, 31, 10)))
+    axes[1, 0].set_ylabel('Energy sub metering')
+    axes[1, 0].legend()
+
+    axes[1, 1].plot(df.datetime, df.Global_reactive_power, c='black')
+    axes[1, 1].set_yticks(np.arange(0, 0.6, 0.1))
+    axes[1, 1].set_ylabel('Global_reactive_power')
+    axes[1, 1].set_xlabel('datetime')
+
+    fig.savefig('plot4.png')
+
+if __name__ == "__main__":
+    # define some global variable for ploting
+    date_fmt = mdates.DateFormatter('%a')
+    day_locator = mdates.DayLocator(interval=1)
+    
+    # construct plots
+    plot1()
+    plot2()
+    plot3()
+    plot4()
